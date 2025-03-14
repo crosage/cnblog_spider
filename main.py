45fe5f7369b48cd19bc876409ac13d8e53a83fca
水口钳 <90540469+crosage@users.noreply.github.com>
2025-03-14 11:37:25 +0800

    :sparkles: feat: 直接保存为markdown格式+放弃使用异步以保证时间顺序

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
-import asyncio
-
+import requests
 from bs4 import BeautifulSoup
-import aiohttp
-import aiofiles
+import html2text
+import os
 
-path=""
-user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50"
-cookies={
-    ".CNBlogsCookie":"your cookie",
-    ".Cnblogs.AspNetCore.Cookies":"your cookie"
+path = "D:\\myblog\\tmp\\"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50"
+cookies = {
+    ".CNBlogsCookie": " ",
+    ".Cnblogs.AspNetCore.Cookies": " ",
 }
-prefix="https://www.cnblogs.com/znsbc-13/p/"
-async def cnblogs_content(urls:[]):
-    index:int=0
-    async with aiohttp.ClientSession() as session:
-        for url in urls:
-            index = index + 1
-            try:
-                async with session.get(url=url) as resp:
-                    if resp.status==200:
-                        _bytes=await resp.text()
-                        soup=BeautifulSoup(_bytes,features="html.parser")
-                        body=soup.find("div",id="cnblogs_post_body")
-                        head=soup.find("a",id="cb_post_title_url").get_text()
-                        print(f"正在处理第{index}篇博文")
-                        async with aiofiles.open(path+str(index)+".html","w",encoding="utf-8") as file:
-                            await file.write(str(body))
-            except:
-                print(f"处理第{index}出现问题 url为{url}")
+prefix = "https://www.cnblogs.com/znsbc-13/p/"
 
-async def geturls():
-    url="https://i.cnblogs.com/api/posts/list"
-    urls=[]
-    async with aiohttp.ClientSession() as session:
-        pages=10000
-        for _index in range(1,pages):
-            params = {
-                "p": _index,
-                "t": 1,
-                "cfg": 0
-            }
-            async with session.get(url=url,params=params,cookies=cookies,proxy="http://localhost:7890") as resp:
-                _json=await resp.json()
-                counts=int(_json["postsCount"])
-                pageSize=int(_json["pageSize"])
-                for i in _json["postList"]:
-                    urls.append(prefix+str(i["id"])+".html")
-                if pageSize*_index>=counts:
-                    break
+def cnblogs_content(urls):
+    index = 0
+    session = requests.Session()
+    for url in urls:
+        index += 1
+        try:
+            resp = session.get(url=url,proxies={"http": "http://localhost:7890", "https": "http://localhost:7890"})
+            if resp.status_code == 200:
+                soup = BeautifulSoup(resp.text, features="html.parser")
+                body = soup.find("div", id="cnblogs_post_body")
+                title = soup.find("a", id="cb_post_title_url").get_text().strip()
+                date_element = soup.find("a", id="cb_post_title_url")
+                date_title = date_element.get('title').replace("发布于", "")
+                print(f"正在处理第{index}篇博文 标题为{title} 发布时间{date_title}")
+                title=title.replace("/", "_")
+                title=title.replace("\\", "_")
+                h = html2text.HTML2Text()
+                markdown_content = h.handle(str(body))
+                markdown_content = f"---\ntitle: {title}\n---\n\n归档于 {date_title}\n\n{markdown_content}\n\n"
+                with open(os.path.join(path, title + ".md"), "w", encoding="utf-8") as file:
+                    file.write(markdown_content)
+        except Exception as e:
+            print(f"处理第{index}出现问题 url为{url}, 错误信息：{e}")
+
+def geturls():
+    url = "https://i.cnblogs.com/api/posts/list"
+    urls = []
+    session = requests.Session()
+    pages = 10000
+    for _index in range(1, pages):
+        params = {
+            "p": _index,
+            "t": 1,
+            "cfg": 0,
+        }
+        resp = session.get(url=url, params=params, cookies=cookies,proxies={"http": "http://127.0.0.1:7890", "https": "http://127.0.0.1:7890"})
+        _json = resp.json()
+        counts = int(_json["postsCount"])
+        pageSize = int(_json["pageSize"])
+        for i in _json["postList"]:
+            urls.append(prefix + str(i["id"]) + ".html")
+        if pageSize * _index >= counts:
+            break
     print(f"共{len(urls)}篇博文")
-    await cnblogs_content(urls)
-path=input("输入要保存的文件路径\n")
-asyncio.run(geturls())
+    urls.reverse()
+    cnblogs_content(urls)
+
+path = "D:\\myblog\\tmp\\"
+geturls()
